7102bfe1c3a4d35cf72989fc825a9900cde59975
Dan McCreary <dan.mccreary@gmail.com>
2025-07-21 12:07:48 -0500

    updating content

---
+++ src/led-strip-power-test.py
@@ -0,0 +1,46 @@
+# Power Test
+# Draw all the pixels a specific color and intensity and then watch the current
+# Use a USB current meter
+from machine import Pin
+from neopixel import NeoPixel
+from utime import sleep
+# get the configuration information
+import config
+
+np = config.NUMBER_PIXELS
+strip = NeoPixel(Pin(config.NEOPIXEL_PIN), np)
+
+# give enough time for the current meter to stabilize
+sleep_time = 5
+while True:
+    
+    # make the strip red
+    for i in range(0,np):
+        strip[i] = (255,0,0)
+        strip.write()
+    sleep(sleep_time)
+   
+    # make the strip green
+    for i in range(0,np):
+        strip[i] = (0,255,0)
+        strip.write()
+    sleep(sleep_time)
+    
+    # make the strip blue
+    for i in range(0,np):
+        strip[i] = (0,0,255)
+        strip.write()
+    sleep(sleep_time)
+    
+    # make the strip white
+    for i in range(0,np):
+        strip[i] = (255,255,255)
+        strip.write()
+    sleep(sleep_time)
+    
+    # turn off the strip
+    for i in range(0,np):
+        strip[i] = (0,0,0)
+        strip.write()
+        sleep(.05)
+    sleep(sleep_time)
